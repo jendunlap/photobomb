5b8dc053aa7ae144e80e39becd9b5b9c8e17f6f9
Jen Dunlap <jenniferleighdunlap@gmail.com>
2023-08-31 17:06:37 -0400

    fixed issue with react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>Hello!</p>
-      </header>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </main>
     </div>
   )
 }
