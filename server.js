fa62169d88a64806b5f464f90e8e495c3bca91d5
Jen Dunlap <jenniferleighdunlap@gmail.com>
2023-08-31 16:26:23 -0400

    modification to server

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const routes = require('./routes')
+const db = require('./db')
+// const auth = require('./routes/auth')
+const cors = require('cors')
+
+const PORT = process.env.PORT || 3001
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+app.use(express.static(`${__dirname}/client/build`))
+
+app.use('/', routes)
+// app.use('/auth', auth)
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
+app.get('/*', (req, res) => {
+  res.sendFile(`${__dirname}/client/build/index.html`)
+})
+app.listen(PORT, () => console.log(`❤️‍🔥 listening on port: ${PORT} ❤️‍🔥`))
