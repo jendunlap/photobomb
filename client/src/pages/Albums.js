0b74cd91e413589a535bd1b953b501b89380cca9
Jen Dunlap <jenniferleighdunlap@gmail.com>
2023-10-12 12:04:04 -0400

    created beginnings of albums grid

--- client/src/pages/Albums.js
+++ client/src/pages/Albums.js
@@ -1,7 +1,43 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
+import axios from 'axios'
+import Album from '../components/Album'
 
 const Albums = () => {
-  return <div className="albums">Albums</div>
+  const [albums, setAlbums] = useState([])
+
+  let navigate = useNavigate()
+  let { albumId } = useParams()
+
+  const getAlbums = async () => {
+    const response = await axios.get(`/albums`)
+    setAlbums(response.data.albums)
+    console.log(response.data.albums)
+  }
+
+  const viewAlbum = (id) => {
+    navigate(`/albumss/${id}`)
+  }
+
+  useEffect(() => {
+    getAlbums()
+  }, [albumId])
+
+  return (
+    <div className="albumsGrid">
+      {albums
+        .sort((a, b) => a.name.localeCompare(b.name))
+        .map((album) => (
+          <Album
+            id={album._id}
+            key={album._id}
+            image={album.image}
+            name={album.name}
+            onClick={viewAlbum}
+          />
+        ))}
+    </div>
+  )
 }
 
 export default Albums
