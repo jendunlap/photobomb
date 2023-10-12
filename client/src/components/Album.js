4df55dc8d2dfc296e4bac9430167490b52aec531
Jen Dunlap <jenniferleighdunlap@gmail.com>
2023-10-12 11:55:58 -0400

    beginning set up album.js

--- client/src/components/Album.js
+++ client/src/components/Album.js
@@ -1,7 +1,16 @@
 import React from 'react'
 
-const Album = () => {
-  return <div>Album</div>
+const Album = (props) => {
+  return (
+    <div className="albumDiv" onClick={() => props.onClick(props.id)}>
+      <div className="imgageWrapper">
+        <img className="albumImage" src={props.image} alt={props.name}></img>
+      </div>
+      <div className="infoWrapper">
+        <h1 className="albumName">{props.name}</h1>
+      </div>
+    </div>
+  )
 }
 
 export default Album
