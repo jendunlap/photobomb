999c45f28f5b6d8bbf86542d9f4af897afea9cc4
Jen Dunlap <jenniferleighdunlap@gmail.com>
2023-10-12 12:16:03 -0400

    create page initialized

---
+++ client/src/pages/Create.js
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import Grid from '../components/Grid'
+import Hero from '../components/Hero'
+import Image from '../components/Image'
+import Images from '../components/Image'
+import Text from '../components/Text'
+
+const Create = () => {
+  let navigate = useNavigate()
+
+  return <div>Create</div>
+}
+
+export default Create
