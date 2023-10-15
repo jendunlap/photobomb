c54305a4457837e9cd9d4bbf2d79aaa42c3ee2c0
Jen Dunlap <jenniferleighdunlap@gmail.com>
2023-10-15 13:56:53 -0400

    added fields to modify component

--- client/src/pages/Modify.js
+++ client/src/pages/Modify.js
@@ -7,7 +7,12 @@ const Modify = () => {
   let { albumId } = useParams()
   let navigate = useNavigate()
 
+  const initialState = {
+    name: ''
+  }
+
   const [albumComponents, setAlbumComponents] = useState([])
+  const [formState, setFormState] = useState(initialState)
 
   const addComponent = (type) => {
     const newComponent = {
@@ -30,6 +35,17 @@ const Modify = () => {
     getAlbumInfo()
   }, [albumId])
 
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+    await axios.put(`/albums/${albumId}`, formState)
+    setFormState(initialState)
+    navigate(`/albums/${albumId}`)
+  }
+
+  const handleChange = (event) => {
+    setFormState({ ...formState, [event.target.id]: event.target.value })
+  }
+
   const editComponent = (index, updatedContent) => {
     const updatedComponents = [...albumComponents]
     updatedComponents[index].content = updatedContent
@@ -38,19 +54,31 @@ const Modify = () => {
 
   return (
     <div>
-      <button onClick={() => addComponent('Grid')}>Add Grid</button>
-      <button onClick={() => addComponent('Hero')}>Add Hero</button>
-      <button onClick={() => addComponent('Image')}>Add Image</button>
-      <button onClick={() => addComponent('Images')}>Add Images</button>
-      <button onClick={() => addComponent('Text')}>Add Text</button>
-
-      {albumComponents.map((component, index) => (
-        <Registry
-          key={index}
-          component={component}
-          onEdit={(updatedContent) => editComponent(index, updatedContent)}
+      <form className="modifyForm" onSubmit={handleSubmit}>
+        <label htmlFor="name">name your album!</label>
+        <input
+          onChange={handleChange}
+          type="text"
+          id="name"
+          value={formState.name}
         />
-      ))}
+        <button onClick={() => addComponent('Grid')}>Add Grid</button>
+        <button onClick={() => addComponent('Hero')}>Add Hero</button>
+        <button onClick={() => addComponent('Image')}>Add Image</button>
+        <button onClick={() => addComponent('Images')}>Add Images</button>
+        <button onClick={() => addComponent('Text')}>Add Text</button>
+
+        {albumComponents.map((component, index) => (
+          <Registry
+            key={index}
+            component={component}
+            onEdit={(updatedContent) => editComponent(index, updatedContent)}
+          />
+        ))}
+      </form>
+      <button className="submitButton" type="submit" onClick={handleSubmit}>
+        make changes
+      </button>
     </div>
   )
 }
