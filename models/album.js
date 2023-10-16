2cc16ab85050c2c531f41cfbf9db3143008c957b
Jen Dunlap <jenniferleighdunlap@gmail.com>
2023-10-16 10:10:24 -0400

    changes to album model

--- models/album.js
+++ models/album.js
@@ -5,7 +5,11 @@ const Album = new Schema(
   {
     name: { type: String, required: false },
     image: { type: String, required: false },
-    grids: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Grid' }]
+    grids: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Grid' }],
+    heros: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Hero' }],
+    images: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Image' }],
+    images2: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Images' }],
+    text: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Text' }]
   },
   { timestamps: true }
 )
